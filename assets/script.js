5f5732839bbe05e8eb34ee4e1217335338368b4f
Mohammad <66128138+ghasemigol@users.noreply.github.com>
2026-06-02 17:34:49 -0400

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-document.querySelectorAll(".card, .project-grid article, .person").forEach((el)=>{
+document.querySelectorAll(".card, .project-grid article, .person, .map-node").forEach((el)=>{
   el.addEventListener("mousemove", e=>{
     const r=el.getBoundingClientRect();
-    el.style.transform=`perspective(700px) rotateX(${-(e.clientY-r.top-r.height/2)/35}deg) rotateY(${(e.clientX-r.left-r.width/2)/35}deg)`;
+    el.style.setProperty("--mx", `${e.clientX-r.left}px`);
+    el.style.setProperty("--my", `${e.clientY-r.top}px`);
   });
-  el.addEventListener("mouseleave",()=>{el.style.transform="none";});
 });
